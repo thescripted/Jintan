566594f637e136443e5525d3d66e9f2c61dc0ba2
thescripted <somethingscripted@gmail.com>
2021-03-13 13:05:21 -0600

    set static root config settings & add stub static file

--- jintan/jintan/settings.py
+++ jintan/jintan/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,5 +155,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static'),
+)
